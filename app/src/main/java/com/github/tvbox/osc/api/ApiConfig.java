a687d12e8d400ef32eb43fff73ba365863ec0130
wo13468733 <33351138+wo13468733@users.noreply.github.com>
2022-10-23 11:44:43 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/wo13468733/666/raw/branch/master/xb.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
